cf0bf6adbebce399ba44a1e5e62e0ca90149d0c7
Tim Sell <trsell@gmail.com>
2015-01-17 23:08:30 +1100

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,11 @@ set -e
 gulp clean
 gulp dist
 echo shootme.timothysell.com > dist/CNAME
+
+echo "Committing changes to dist dir"
 git add --all dist
 git commit -m"deploy dist"
+
+echo "Pushing to github"
 git subtree push --prefix dist github gh-pages
 
