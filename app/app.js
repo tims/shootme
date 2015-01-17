b8a3d97f19cd7f6c231855c92231bb27dbcc733a
Tim Sell <trsell@gmail.com>
2015-01-17 19:01:34 +1100

    things

--- app/app.js
+++ app/app.js
@@ -29,7 +29,7 @@ angular.module('shootme', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'n
 
 //'https://shootme-api.herokuapp.com/api'
 angular.module('shootme').value('configuration', {
-  apiUrl: 'http://localhost:8001/api'
+  apiUrl: 'https://shootme-api.herokuapp.com/api'
 });
 //'http://localhost:8001/api'
 
